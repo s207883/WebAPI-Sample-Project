94e3c3924d21d7d1c3a435a47c2a1b0198ff3932
s207883 <s207883@yandex.ru>
2019-11-15 16:29:40 +0300

    Добавлена модель сотрудника

---
+++ LogTZ.DAL/Model/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogTZ.DAL.Model
+{
+	/// <summary>
+	/// Модель сотрудника.
+	/// </summary>
+	public class Customer
+	{
+		/// <summary>
+		/// ИД сотрудника.
+		/// </summary>
+		public int CustomerId { get; set; }
+
+		/// <summary>
+		/// Имя сотрудника.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Дата рождения.
+		/// </summary>
+		public DateTime BirthDate { get; set; }
+	}
+}
