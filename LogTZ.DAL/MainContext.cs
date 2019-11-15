41919f49aa9575d7a0612d8f922b40cfc74f2eec
s207883 <s207883@yandex.ru>
2019-11-15 19:01:46 +0300

    Добавлена строка подключения БД.

--- LogTZ.DAL/MainContext.cs
+++ LogTZ.DAL/MainContext.cs
@@ -28,6 +28,11 @@ namespace LogTZ.DAL
 			Database.EnsureCreated ( );
 		}
 
+		protected override void OnConfiguring ( DbContextOptionsBuilder optionsBuilder )
+		{
+			optionsBuilder.UseSqlServer ( @"Server=(localdb)\mssqllocaldb;Database=LogroconDB;Integrated Security=true;" );
+			base.OnConfiguring ( optionsBuilder );
+		}
 		/// <summary>
 		/// Реализация FluentAPI.
 		/// </summary>
