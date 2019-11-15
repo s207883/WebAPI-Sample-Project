939ce199f8dce7be991b973b3afad18c68a2a8c8
s207883 <s207883@yandex.ru>
2019-11-15 16:33:32 +0300

    Добавлена модель должности.

---
+++ LogTZ.DAL/Model/Position.cs
@@ -0,0 +1,23 @@
+﻿namespace LogTZ.DAL.Model
+{
+	/// <summary>
+	/// Модель должности.
+	/// </summary>
+	public class Position
+	{
+		/// <summary>
+		/// ИД должности.
+		/// </summary>
+		public int PositionId { get; set; }
+
+		/// <summary>
+		/// Название должности.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Вес должности (грейд).
+		/// </summary>
+		public int Grade { get; set; }
+	}
+}
