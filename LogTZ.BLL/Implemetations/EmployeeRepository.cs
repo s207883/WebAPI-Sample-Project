ddf3534ff0e511515e373ddb1fc4c389522951c9
s207883 <s207883@yandex.ru>
2019-11-15 18:54:10 +0300

    Частичная реализация EmployeeRepository

---
+++ LogTZ.BLL/Implemetations/EmployeeRepository.cs
@@ -0,0 +1,97 @@
+﻿using AutoMapper;
+using LogTZ.BLL.Interfaces;
+using LogTZ.Core.EditModels;
+using LogTZ.Core.Enums;
+using LogTZ.Core.ViewModels;
+using LogTZ.DAL;
+using LogTZ.DAL.Model;
+using System.Linq;
+
+namespace LogTZ.BLL.Implemetations
+{
+	public class EmployeeRepository : IEmployeeRepository
+	{
+		private readonly MainContext _mainContext;
+		private readonly IMapper _mapper;
+
+		public EmployeeRepository ( MainContext mainContext, IMapper mapper )
+		{
+			_mainContext = mainContext;
+			_mapper = mapper;
+		}
+
+		public RepositoryActionsResult CreateEmployee ( EmployeeEditModel employeeEditModel )
+		{
+			if ( employeeEditModel is null )
+			{
+				return RepositoryActionsResult.DadData;
+			}
+
+			var employeeModel = _mapper.Map<Employee> ( employeeEditModel );
+
+			_mainContext.Add ( employeeModel );
+			_mainContext.SaveChanges ( );
+
+			return RepositoryActionsResult.Success;
+		}
+
+		public RepositoryActionsResult DeleteEmployeeById ( int employeeId )
+		{
+			var employeeInDb = _mainContext.Employees.FirstOrDefault ( emp => emp.EmployeeId == employeeId );
+
+			if ( employeeInDb == default )
+			{
+				return RepositoryActionsResult.DadData;
+			}
+			else
+			{
+				//TODO: Добавить проверку.
+				_mainContext.Employees.Remove ( employeeInDb );
+				_mainContext.SaveChanges ( );
+
+				return RepositoryActionsResult.Success;
+			}
+		}
+
+		public (RepositoryActionsResult repositoryActionResult, EmployeeViewModel employeeViewModel) GetEmployeeById ( int employeeId )
+		{
+			var employeeModel = _mainContext.Employees.FirstOrDefault ( emp => emp.EmployeeId == employeeId );
+
+			if ( employeeModel == default )
+			{
+				return (RepositoryActionsResult.DadData, default);
+			}
+			else
+			{
+				var employeeViewModel = _mapper.Map<EmployeeViewModel> ( employeeModel );
+				return (RepositoryActionsResult.Success, employeeViewModel);
+			}
+		}
+
+		public RepositoryActionsResult UpdateEmployee ( EmployeeEditModel employeeEditModel )
+		{
+			if ( employeeEditModel is null )
+			{
+				return RepositoryActionsResult.DadData;
+			}
+
+			var employeeModel = _mapper.Map<Employee> ( employeeEditModel );
+
+			var employeeInDb = _mainContext.Employees.FirstOrDefault ( emp => emp.EmployeeId == employeeModel.EmployeeId );
+
+			if ( employeeInDb == default )
+			{
+				return RepositoryActionsResult.DadData;
+			}
+			else
+			{
+				employeeInDb.Name = employeeModel.Name;
+				employeeInDb.BirthDate = employeeInDb.BirthDate;
+
+				_mainContext.SaveChanges ( );
+
+				return RepositoryActionsResult.Success;
+			}
+		}
+	}
+}
