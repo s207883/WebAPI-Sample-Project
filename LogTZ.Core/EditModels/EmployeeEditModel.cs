deb92318c757cc5c2df269d662ee4ad00c2a839c
s207883 <s207883@yandex.ru>
2019-11-18 18:57:11 +0300

    Реализованы EmployeeEditModel, PositionEditModel

--- LogTZ.Core/EditModels/EmployeeEditModel.cs
+++ LogTZ.Core/EditModels/EmployeeEditModel.cs
@@ -6,5 +6,19 @@ namespace LogTZ.Core.EditModels
 {
 	public class EmployeeEditModel
 	{
+		/// <summary>
+		/// ИД сотрудника.
+		/// </summary>
+		public int EmployeeId { get; set; }
+
+		/// <summary>
+		/// Имя сотрудника.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Дата рождения.
+		/// </summary>
+		public DateTime BirthDate { get; set; }
 	}
 }
--- LogTZ.Core/EditModels/PositionEditModel.cs
+++ LogTZ.Core/EditModels/PositionEditModel.cs
@@ -6,6 +6,19 @@ namespace LogTZ.Core.EditModels
 {
 	public class PositionEditModel
 	{
+		/// <summary>
+		/// ИД должности.
+		/// </summary>
+		public int PositionId { get; set; }
 
+		/// <summary>
+		/// Название должности.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Вес должности (грейд).
+		/// </summary>
+		public int Grade { get; set; }
 	}
 }
