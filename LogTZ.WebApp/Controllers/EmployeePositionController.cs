7d7cc06371ab49f5d44026659ae0c88810dc319c
s207883 <s207883@yandex.ru>
2019-11-18 19:09:40 +0300

    Добавлен EmployeePositionController

---
+++ LogTZ.WebApp/Controllers/EmployeePositionController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LogTZ.BLL;
+using LogTZ.Core.Enums;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogTZ.WebApp.Controllers
+{
+	[Route ( "api/[controller]" )]
+	[ApiController]
+	public class EmployeePositionController : ControllerBase
+	{
+		//TODO: Документация.
+		private readonly RepoManager _repoManager;
+
+		public EmployeePositionController (RepoManager repoManager)
+		{
+			_repoManager = repoManager;
+		}
+
+		[HttpPost]
+		public ActionResult SetPositionToEmployee (int positionId, int employeeId)
+		{
+			var result = _repoManager.EployeePositionRepository.SetPositionToEmployee(positionId,employeeId);
+
+			if ( result == RepositoryActionsResult.Success )
+			{
+				return Ok();
+			}
+			else
+			{
+				return BadRequest(result);
+			}
+		}
+
+		[HttpDelete]
+		public ActionResult RemovePositionFromEmployee (int positionId, int employeeId)
+		{
+			var result = _repoManager.EployeePositionRepository.RemovePositionFromEmployee ( positionId, employeeId );
+
+			if ( result == RepositoryActionsResult.Success )
+			{
+				return Ok ( );
+			}
+			else
+			{
+				return BadRequest ( result );
+			}
+		}
+
+	}
+}
