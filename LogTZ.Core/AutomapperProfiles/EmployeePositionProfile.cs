9a6362ba8724d9f0b040a53f917438e1b1ae8ee1
s207883 <s207883@yandex.ru>
2019-11-18 18:09:02 +0300

    Добавлен профиль автомаппера EmployeePosition. Добавлена EmployeePositionEditModel.

---
+++ LogTZ.Core/AutomapperProfiles/EmployeePositionProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using LogTZ.Core.EditModels;
+using LogTZ.Core.ViewModels;
+using LogTZ.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogTZ.Core.AutomapperProfiles
+{
+	public class EmployeePositionProfile : Profile
+	{
+		public EmployeePositionProfile ( )
+		{
+			CreateMap<EmployeePosition, EmployeePositionViewModel>().ReverseMap();
+			CreateMap<EmployeePosition, EmployeePositionEditModel>().ReverseMap();
+		}
+	}
+}
---
+++ LogTZ.Core/EditModels/EmployeePositionViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace LogTZ.Core.EditModels
+{
+	/// <summary>
+	/// Модель представления должности сотрудника.
+	/// </summary>
+	public class EmployeePositionEditModel
+	{
+		/// <summary>
+		/// ИД сотрудника.
+		/// </summary>
+		public int EmployeeId { get; set; }
+
+		/// <summary>
+		/// Должность сотрудника.
+		/// </summary>
+		public int PositionId { get; set; }
+	}
+}
